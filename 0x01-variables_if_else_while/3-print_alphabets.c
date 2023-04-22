59bc1854595d4a9242f2741474f5af36e34a12ce
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-21 19:29:18 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,18 +14,18 @@ char ch = 'a';
 char CH = 'A';
 /*prints a - z*/
 while (ch <= 'z')
-{ 
+{
 putchar (ch);
 ch++;
 }/*prints A - Z*/
 while (CH <= 'Z')
 {
 putchar (CH);
-CH++;    
+CH++;
 }
 
 putchar('\n');
 
 return (0);
-		
+
 }
