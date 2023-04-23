1404484901c8cb04dc0b4722493126e344582285
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-22 19:39:22 -0700

    a

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
 * main - Entry point
-* Description: prints all single digit numbers
+* Description: prints all single digit numbers of base 10 starting from 0
 * Return: Always 0 (Success)
 */
 
