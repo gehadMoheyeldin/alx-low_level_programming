d9c477af790bbd7afe5939369ff1dfc67157590e
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-22 17:54:22 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints all  alphabet in lowercase except e and q
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("i%", digit);
+digit++;
+}
+printf('\n');
+return (0);
+}
