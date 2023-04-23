fdf5eb4660f8076c5fdacbf6ba805b4d390a5f61
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-23 08:29:56 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,21 @@
 */
 int main(void)
 {
-int digit1 = 0,
+int digit1 = 0;
 int digit2, digit3;
 while (digit1 <= 9)
 {
-digit2 =
+digit2 = 0;
 while (digit2 <= 9)
 {
+digit3 = 0;
 while (digit3 <= 9)
 {
 if (digit1 != digit2 &&
 digit1 < digit2 &&
 digit2 != digit3 &&
 digit2 < digit3)
+{
 putchar(digit1 + 48);
 putchar(digit2 + 48);
 putchar(digit3 + 48);
@@ -27,6 +29,8 @@ if  (digit1 + digit2 + digit3 != 24)
 putchar(',');
 putchar(' ');
 }
+
+}
 digit3++;
 }
 digit2++;
@@ -34,5 +38,6 @@ digit2++;
 digit1++;
 }
 putchar('\n');
+
 return (0);
 }
