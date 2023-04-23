2f39794d4110423a57f4c0706dda24331e218ff4
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-22 21:40:48 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 int digit1 = 0,  digit2;
 
-while (digit <= 9)
+while (digit1 <= 9)
 {
 digit2 = 0;
 while (digit2 <= 9)
@@ -22,8 +22,8 @@ putchar(digit1 + 48);
 putchar(digit2 + 48);
 if  (digit1 + digit2 != 17)
 {
-putchar(',')
-putchar(' ')
+putchar(',');
+putchar(' ');
 }
 }
 digit2++;
