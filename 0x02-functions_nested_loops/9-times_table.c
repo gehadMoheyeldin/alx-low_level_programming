aa0aeb59db557eb9cbb7d51a4c541239b200761a
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-25 18:02:30 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,19 @@ for (num = 0; num <= 9; num++)
 _putchar(48);
 for (mult = 1; mult <= 9; mult++)
 {
-_putchar('.')
-_putchar(' ')
+_putchar('.');
+_putchar(' ');
 prod = num * mult;
 /*
  * put space if product is a single number
  * place the first digit if its tow numbers
  */
 if (prod <= 9)
-_putchar(' ')
+_putchar(' ');
 else
 _putchar((prod / 10) + 48);/*get the first digit*/
 _putchar((prod % 10) + 48);/*get the second  digit*/
 }
-_putchar('\n')
+_putchar('\n');
 }
 }
