a9d47a214920a887a16cce2347bb123681cb63d6
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-04-27 16:10:58 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,25 @@
 
 void print_triangle(int size)
 {
+
 int hght, base;
 if (size <= 0)
 _putchar('\n');
+
 else
 {
-for (hgt = 1; hght <= size; hght++)
+for (hght = 1; hght <= size; hght++)
 {
 for (base = 1; base <= size; base++)
 {
 if ((hght + base) <= size)
 _putchar(' ');
+
 else
 _putchar('#');
 }
 _putchar('\n');
+
 }
 }
 }
