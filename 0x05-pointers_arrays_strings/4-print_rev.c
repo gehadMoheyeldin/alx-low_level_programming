afca3556ef4ce6e1feab935982350ecfd3b57bb4
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-02 16:03:16 -0700

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,18 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
+
+int i = 0;
 
 
-	while (s[i])
-	       i++;
+while (s[i])
+	i++;
 
-	while (i--)
-	{
-		_putchar(s[i]);
-	}
+while (i--)
+{
+_putchar(s[i]);
+}
 
-	_putchar('\n');
+_putchar('\n');
+
 }
