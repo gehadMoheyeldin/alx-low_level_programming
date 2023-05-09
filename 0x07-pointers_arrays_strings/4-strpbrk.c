60a4eb13a778d78107b70abc677f3e1153eb3042
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-08 19:18:40 -0700

    A

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,9 +4,8 @@
  * _strpbrk - prints the consecutive caracters of any of a set of bytes.
  * @s: source string
  * @accept: searching string
- *
  * Return: new string.
- */ 
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -19,7 +18,7 @@ for (j = 0; *(accept + j); j++)
 if (*(s + r) == *(accept + j))
 {
 break;
-                        }
+}
 }
 if (*(accept + j) != '\0')
 {
