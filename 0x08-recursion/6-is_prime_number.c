8f9c4d68ca9b21f5366d66b6ea0d29b6687e994d
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-12 11:44:30 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,18 +8,19 @@
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		return (is_prime_manual(n, 2));
-	}
+	
+if (n <= 1)
+{
+return (0);
+}
+else if (n == 2)
+{
+return (1);
+}
+else
+{
+return (is_prime_manual(n, 2));
+}
 }
 
 /**
@@ -31,16 +32,16 @@ int is_prime_number(int n)
 
 int is_prime_manual(int n, int i)
 {
-	if (n % i == 0)
-	{
-		return (0);
-	}
-	else if (i == (n - 1) && n % i != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (is_prime_manual(n, i + 1));
-	}
+if (n % i == 0)
+{
+return (0);
+}
+else if (i == (n - 1) && n % i != 0)
+{
+return (1);
+}
+else
+{
+return (is_prime_manual(n, i + 1));
+}
 }
