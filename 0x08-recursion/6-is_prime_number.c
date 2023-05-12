0baeca53ee7a16a5194f6c4f6803f53200ac3ad2
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-12 11:38:09 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,11 @@
+#include "main.h"
+
 /**
  * is_prime_number - return the sqrt of a natural number.
  * @n: natural number
  * Return: 1 if is prime otherwise 0.
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -18,12 +21,14 @@ int is_prime_number(int n)
 		return (is_prime_manual(n, 2));
 	}
 }
+
 /**
  * is_prime_manual - calculate if the number is prime.
  * @n: natural number
  * @i: counter or divisor of n.
  * Return: 1 if is prime 0 otherwise.
  */
+
 int is_prime_manual(int n, int i)
 {
 	if (n % i == 0)
