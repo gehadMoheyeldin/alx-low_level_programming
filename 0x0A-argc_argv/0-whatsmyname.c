9d12bb28eca24cfb6fd98e7d8837d1a55866425f
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-12 17:28:43 -0700

    finished

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program  name of the executable, followed by a new line 
+ * @argc: int
+ * @argv: list
+ * Return: always 0.
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
