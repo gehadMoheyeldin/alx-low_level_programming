db2f34fd4cbe06ccd93d15db414031002f4a81b8
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-12 18:16:03 -0700

    finished

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,27 +5,27 @@
  * main - print the addition of two positive numbers.
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0 .
+ * Return: Always 0
  */
 
 int main(int argc, char **argv)
 {
-	int sum, i, j;
+int sum, i, j;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[i]);
-	}
+sum = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
 
-	printf("%d\n", sum);
-	return (0);
+printf("%d\n", sum);
+return (0);
 }
