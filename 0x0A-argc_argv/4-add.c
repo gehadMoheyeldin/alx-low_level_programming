5961395bf7b08c24465926c207898a04614b04fb
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-12 19:13:00 -0700

    finished

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - print the addition of two positive numbers.
@@ -11,32 +13,35 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
 
-	if (argc < 2)
-		printf("0\n");
-	else
-	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
-		{
-			ptr = argv[i];
-			length = strlen(ptr);
+int i, j, length, sum;
+char *ptr;
 
-			for (j = 0; j < length; j++)
-			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
+if (argc < 2)
+printf("0\n");
+else
+{
+sum = 0;
+for (i = 1; i < argc; i++)
+{
+ptr = argv[i];
+length = strlen(ptr);
 
-			sum += atoi(argv[i]);
-		}
+for (j = 0; j < length; j++)
+{
+if (isdigit(*(ptr + j)) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
 
-	printf("%d\n", sum);
-	}
-	return (0);
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+}
+
+return (0);
+
 }
