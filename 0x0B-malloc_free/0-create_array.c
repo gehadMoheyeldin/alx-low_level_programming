4f6a986358d402fc23ffa0e51dfd3758af817188
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-17 16:53:55 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and initializes it 
+ * @size: size of array
+ * @c: char to initialize
+ * Return: pointer to the array or NULL 
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *pstr;
+	unsigned int i;
+
+	pstr = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		pstr[i] = c;
+	pstr[i] = 0;
+	return (size < 1 ? 0 : pstr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * _strdup - copy of the  input string in newly allocated memory
+ * @str: string
+ * Return: pointer to duplicate string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *pstr;
+	unsigned int i = 0;
+
+	if (str == 0)
+		return (0);
+	while (str[i] != 0)
+		i++;
+	i++;
+	pstr = malloc(sizeof(char) * i);
+	for (i = 0; str[i] != 0; i++)
+		pstr[i] = str[i];
+	pstr[i] = 0;
+	return (pstr);
+}
