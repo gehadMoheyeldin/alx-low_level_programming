01c4036d785977a0c3e4a0615c652a66cb963168
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-17 17:41:40 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,25 +11,13 @@
 char *create_array(unsigned int size, char c)
 }
 
-char *s;
-	unsigned int i;
+char * = malloc(size);
+if (size == 0 || n == 0)
+return (0);
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
+while (size--)
+n[size] = c;
 
-	s = malloc((size) * sizeof(char));
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
+return (n);
+
 }
