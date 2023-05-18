f9f3f2bf58a44d434634f08466b08e590d66578a
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-17 18:07:49 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,22 +4,28 @@
 /**
  * _strdup - copy of the  input string in newly allocated memory
  * @str: string
- * Return: pointer to duplicate string or NULL
+ * Return: 0
  */
 
 char *_strdup(char *str)
 {
-	char *pstr;
-	unsigned int i = 0;
 
-	if (str == 0)
-		return (0);
-	while (str[i] != 0)
-		i++;
-	i++;
-	pstr = malloc(sizeof(char) * i);
-	for (i = 0; str[i] != 0; i++)
-		pstr[i] = str[i];
-	pstr[i] = 0;
-	return (pstr);
+char *pstr;
+unsigned int i = 0;
+
+if (str == 0)
+return (0);
+
+while (str[i] != 0)
+i++;
+i++;
+
+pstr = malloc(sizeof(char) * i);
+
+for (i = 0; str[i] != 0; i++)
+pstr[i] = str[i];
+
+pstr[i] = 0;
+
+return (pstr);
 }
