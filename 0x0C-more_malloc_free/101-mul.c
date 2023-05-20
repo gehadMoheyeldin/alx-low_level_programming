16c94a3c558a88d35601d7dc05f4539d6568b976
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-19 17:08:46 -0700

    a

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * main - multiplies two positive numbers
@@ -33,5 +32,6 @@ int main(int argc, char *argv[])
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
+
 	return (0);
 }
