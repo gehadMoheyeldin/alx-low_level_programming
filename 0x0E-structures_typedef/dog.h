13633595e13d924fed520a5857ba598674b84201
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-05-22 19:49:13 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,10 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
+void _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
