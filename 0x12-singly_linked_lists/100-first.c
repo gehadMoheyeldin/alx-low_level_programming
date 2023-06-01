13de55b694134e299a1ed62877f96620c3b792e3
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-06-01 16:26:05 -0700

    finished

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,11 @@
  * _constructor - prints before main
  *
  * Return: void
- **/
+ */
 
 void __attribute__ ((constructor)) first()
 {
+
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
