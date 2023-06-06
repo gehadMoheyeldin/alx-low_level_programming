a167904ec05e59ee4f422e4b262fe67eb2462332
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-06-06 07:52:59 -0700

    a

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,23 +2,22 @@
 
 /**
 *pop_listint - deletes the head node of  list
-*@head: head of the list
+*@head: head of the list to the first node
 *
-*Return: value of popped node data
+*Return: value of popped node 
 */
-
 int pop_listint(listint_t **head)
 {
 
 listint_t *popped;
-int content;
+int n;
 
 if (*head == NULL)
 return (0);
 popped = *head;
-content = popped->n;
+n = popped->n;
 free(popped);
 
 *head = (*head)->next;
-return (content);
+return (n);
 }
