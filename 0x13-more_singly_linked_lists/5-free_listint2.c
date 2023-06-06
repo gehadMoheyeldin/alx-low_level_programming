52ca4df5eb06ccc4f005855f57ec848baeff2bae
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-06-06 05:38:54 -0700

    a

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+*free_listint2 - frees a list of ints
+*@head: pointer to the head of the list
+*
+*Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *node;
+listint_t **temp = head;
+
+if (temp != NULL)
+{
+while (*head != NULL)
+{
+node  = *head;
+free(node);
+*head = (*head)->next;
+}
+
+*temp = NULL;
+
+}
+}
