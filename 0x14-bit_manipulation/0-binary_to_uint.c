595dca1a34528e1b51317166eae936e91b96d2ab
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-06-08 19:22:12 -0700

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * @b: the binary number as a string
+ *
+ * Return: the unsigned int form of b
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that prints a number as a binary string
+ * @n :the  number to convert to binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
+}
