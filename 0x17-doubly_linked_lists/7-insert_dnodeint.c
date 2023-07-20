f33b8792b028c05285ba8f7655cb75664562bb88
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-07-20 05:29:15 -0700

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,6 +10,7 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
